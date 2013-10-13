aadcb01ecb75f6553f652b068fc1f659f7f06d60
Håkon Malmedal <hmalmedal@gmail.com>
2013-10-13 21:58:14 +0200

    Beregner prosenter annerledes

--- data.R
+++ data.R
@@ -12,17 +12,19 @@ colnames(valg2013)[6] <- "Kommunestemmer"
 kommunetotal <- aggregate(Kommunestemmer ~ KommuneID, data = valg2013, sum)
 colnames(kommunetotal)[2] <- "Kommunetotal"
 valg2013 <- join(valg2013, kommunetotal)
+valg2013$Kommuneprosent <- valg2013$Kommunestemmer / valg2013$Kommunetotal * 100
 fylkestotal <- aggregate(Kommunestemmer ~ FylkeID, data = valg2013, sum)
 colnames(fylkestotal)[2] <- "Fylkestotal"
-valg2013 <- join(valg2013, fylkestotal)
 fylkesstemmer <- aggregate(Kommunestemmer ~ FylkeID + Parti, data = valg2013,
                            sum)
 colnames(fylkesstemmer)[3] <- "Fylkesstemmer"
+fylkesstemmer <- join(fylkesstemmer, fylkestotal)
+fylkesstemmer$Fylkesprosent <- fylkesstemmer$Fylkesstemmer /
+    fylkesstemmer$Fylkestotal * 100
 valg2013 <- join(valg2013, fylkesstemmer)
 landsstemmer <- aggregate(Kommunestemmer ~ Parti, data = valg2013, sum)
 colnames(landsstemmer)[2] <- "Landsstemmer"
+landsstemmer$Landstotal  <- sum(landsstemmer$Landsstemmer)
+landsstemmer$Landsprosent <- landsstemmer$Landsstemmer /
+    landsstemmer$Landstotal * 100
 valg2013 <- join(valg2013, landsstemmer)
-valg2013$Landstotal <- sum(valg2013$Kommunestemmer)
-valg2013$Kommuneprosent <- valg2013$Kommunestemmer / valg2013$Kommunetotal * 100
-valg2013$Fylkesprosent <- valg2013$Fylkesstemmer / valg2013$Fylkestotal * 100
-valg2013$Landsprosent <- valg2013$Landsstemmer / valg2013$Landstotal * 100
