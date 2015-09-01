521ddf0ed26589c4c9d19ea29032a7a1024a243c
Håkon Malmedal <hmalmedal@gmail.com>
2015-09-01 18:18:13 +0200

    Trenger bare prosent for kommunene

--- data.R
+++ data.R
@@ -4,23 +4,11 @@ library(readr)
 parti <- read_csv("parti.csv")
 
 valg2013 <- read_csv("valg2013.csv", col_types = "_cc_c_i_____") %>%
-  rename(Kommunestemmer = `Stemmer totalt`) %>%
+  rename(Stemmer = `Stemmer totalt`) %>%
   mutate(Fylke = factor(Fylke, levels = unique(Fylke))) %>%
   mutate(Kommune = factor(Kommune, levels = unique(Kommune))) %>%
   mutate(Parti = factor(Parti, levels = parti$Parti,
                         labels = parti$Partinavn)) %>%
   group_by(Kommune) %>%
-  mutate(Kommunetotal = sum(Kommunestemmer)) %>%
-  ungroup() %>%
-  mutate(Kommuneprosent = Kommunestemmer / Kommunetotal * 100) %>%
-  group_by(Fylke) %>%
-  mutate(Fylkestotal = sum(Kommunestemmer)) %>%
-  group_by(Fylke, Parti) %>%
-  mutate(Fylkesstemmer = sum(Kommunestemmer)) %>%
-  ungroup() %>%
-  mutate(Fylkesprosent = Fylkesstemmer / Fylkestotal * 100) %>%
-  group_by(Parti) %>%
-  mutate(Landsstemmer = sum(Kommunestemmer)) %>%
-  ungroup() %>%
-  mutate(Landstotal = sum(Kommunestemmer),
-         Landsprosent = Landsstemmer / Landstotal * 100)
+  mutate(Prosent = Stemmer / sum(Stemmer) * 100) %>%
+  ungroup()
