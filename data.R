36438fd457b34cde98e4183f2d6d9fe371d0d3b8
Håkon Malmedal <hmalmedal@gmail.com>
2016-09-02 16:05:24 +0200

    Bruker forcats

--- data.R
+++ data.R
@@ -1,12 +1,13 @@
 library(dplyr)
+library(forcats)
 library(readr)
 
 parti <- read_csv("parti.csv", col_types = "cc")
 
 valg2013 <- read_csv("valg2013.csv", col_types = "_cc_c_i_____") %>%
   rename(Stemmer = `Stemmer totalt`) %>%
-  mutate(Fylke = factor(Fylke, levels = unique(Fylke))) %>%
-  mutate(Kommune = factor(Kommune, levels = unique(Kommune))) %>%
+  mutate(Fylke = fct_inorder(Fylke)) %>%
+  mutate(Kommune = fct_inorder(Kommune)) %>%
   mutate(Parti = factor(Parti, levels = parti$Parti,
                         labels = parti$Partinavn)) %>%
   group_by(Kommune) %>%
