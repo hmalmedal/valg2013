f92552b037434843d0a7c27260146c96f81ecf86
Håkon Malmedal <hmalmedal@gmail.com>
2017-04-12 15:24:42 +0200

    Bruker as_factor

--- data.R
+++ data.R
@@ -6,8 +6,8 @@ parti <- read_csv("parti.csv", col_types = "cc")
 
 valg2013 <- read_csv("valg2013.csv", col_types = "_cc_c_i_____") %>%
   rename(Stemmer = `Stemmer totalt`) %>%
-  mutate(Fylke = fct_inorder(Fylke)) %>%
-  mutate(Kommune = fct_inorder(Kommune)) %>%
+  mutate(Fylke = as_factor(Fylke)) %>%
+  mutate(Kommune = as_factor(Kommune)) %>%
   mutate(Parti = factor(Parti, levels = parti$Parti,
                         labels = parti$Partinavn)) %>%
   group_by(Kommune) %>%
